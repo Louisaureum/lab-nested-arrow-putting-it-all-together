54d8c1f0b77adac94e8f3334f07009fc3dd00ca6
Louisaureum <kklouis77@gmail.com>
2026-04-15 12:21:36 +0300

    add files

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function createLoginTracker(userinfo) {
+  let attemptCount = 0;
+  return(passworfAttempt) => {
+    attemptCount++;
+    if (passworfAttempt === userinfo.password && attemptCount <= 3) {
+      return 'Login successful';
+    }
+    if (attemptCount > 3) {
+      return 'Account locked';
+    }
+    return 'Login failed';
+  };
 
-
-
+}
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
